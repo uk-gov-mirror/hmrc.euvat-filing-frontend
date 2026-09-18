305b228170a9569135123228cc2b28d07cd53c8b
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-09-18 12:18:44 +0100

    DTR-8180 add missed import

--- app/controllers/imports/ImportDetailsInfoController.scala
+++ app/controllers/imports/ImportDetailsInfoController.scala
@@ -20,7 +20,7 @@ import controllers.actions.*
 import forms.ImportDetailsInfoFormProvider
 import models.requests.DataRequest
 import javax.inject.Inject
-import models.Mode
+import models.{Mode, NormalMode}
 import navigation.Navigator
 import pages.ImportDetailsInfoPage
 import play.api.i18n.{I18nSupport, MessagesApi}
