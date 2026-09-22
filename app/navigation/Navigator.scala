hmrc/euvat-filing-frontend

DTR-8169: RA5.2.2 - What is the SAD number?

app/navigation/Navigator.scala
@@ -38,6 +38,7 @@ class Navigator @Inject() (currencyConfig: CurrencyConfig,
   }
 
   private val normalRoutes: Page => UserAnswers => Call = {
+    case p if Set(SadReferencePage, SadReferenceNumberPage).contains(p) => importJourneyNavigation(NormalMode)(p)
     case p if Set(RefundingCountryPage,
                  RefundingLanguagePage,
                  RefundPeriodPage,
@@ -101,13 +102,27 @@ class Navigator @Inject() (currencyConfig: CurrencyConfig,
     case _                                 => _ => controllers.routes.IndexController.onPageLoad()
   }
 
+  private def importJourneyNavigation(mode: Mode)(page: Page): UserAnswers => Call = page match {
+    case SadReferencePage => userAnswers =>
+      userAnswers.get(SadReferencePage) match {
+        case Some(true)  => importRoutes.SadReferenceNumberController.onPageLoad(mode)
+        case Some(false) => controllers.routes.JourneyRecoveryController.onPageLoad()
+        case _           => controllers.routes.JourneyRecoveryController.onPageLoad()
+      }
+
+    case SadReferenceNumberPage => _ => controllers.routes.JourneyRecoveryController.onPageLoad()
+
+    case _ => _ => controllers.routes.IndexController.onPageLoad()
+  }
+
   private def importsOrPurchaseStart(mode: Mode)(userAnswers: UserAnswers): Call =
     userAnswers.get(PurchaseOrImportPage) match {
-      case Some(PurchaseOrImport.Import) => importRoutes.SadReferenceController.onPageLoad
+      case Some(PurchaseOrImport.Import) => importRoutes.SadReferenceController.onPageLoad(mode)
       case _                            => purchaseRoutes.PurchaseTypeController.onPageLoad(mode)
     }
 
   private val checkRoutes: Page => UserAnswers => Call = {
+    case p if Set(SadReferencePage, SadReferenceNumberPage).contains(p) => importJourneyNavigation(CheckMode)(p)
     case p if Set(RefundingCountryPage,
                  RefundingLanguagePage,
                  RefundPeriodPage,

conf/messages.cy
@@ -543,3 +543,10 @@ singleAdministrativeDocumentReferenceNumberAvailable.heading = Do you have a Sin
 singleAdministrativeDocumentReferenceNumberAvailable.hint = You can usually find this at the top of the import document
 sadReference.error.required = Select yes if you have the Single Administrative Document (SAD) reference number
 singleAdministrativeDocumentReferenceNumberAvailable.error.required = Select yes if you have the Single Administrative Document (SAD) reference number
+
+sadReferenceNumber.title = What is your Single Administrative Document (SAD) reference number?
+sadReferenceNumber.heading = What is your Single Administrative Document (SAD) reference number?
+sadReferenceNumber.hint = Enter the SAD reference number from your import document
+sadReferenceNumber.error.required = Enter your Single Administrative Document (SAD) reference number
+sadReferenceNumber.error.invalid = Enter a SAD reference number in the correct format
+sadReferenceNumber.error.length = SAD reference number must be 18 characters or less

uk-gov-mirror/hmrc.euvat-filing-frontend

app/navigation/Navigator.scala
@@ -51,6 +51,13 @@ class Navigator @Inject() (currencyConfig: CurrencyConfig,
     case PurchaseOrImportPage              => userAnswers => navigateFromPurchaseOrImportPage(userAnswers)
     case ImportTypePage                    => userAnswers => navigateFromImportTypePage(NormalMode)(userAnswers)
     case ImportSubCodePage                 => userAnswers => navigateFromImportSubCodePage(NormalMode)(userAnswers)
+    case SadReferencePage                  => userAnswers =>
+      userAnswers.get(SadReferencePage) match {
+        case Some(true)  => importRoutes.SadReferenceNumberController.onPageLoad(NormalMode)
+        case Some(false) => controllers.routes.JourneyRecoveryController.onPageLoad()
+        case _           => controllers.routes.JourneyRecoveryController.onPageLoad()
+      }
+    case SadReferenceNumberPage            => _ => controllers.routes.JourneyRecoveryController.onPageLoad()
     case PurchaseTypePage                  => userAnswer => navigateFromPurchaseTypePage(NormalMode)(userAnswer)
     case PurchaseSubCategoryPage           => userAnswers => navigateFromPurchaseSubCategoryPage(NormalMode, userAnswers)
     case DescribeItemsOnInvoicePage        => _ => purchaseRoutes.InvoiceTypeController.onPageLoad(NormalMode)
@@ -81,6 +88,13 @@ class Navigator @Inject() (currencyConfig: CurrencyConfig,
     case CheckYourStateDetailsPage         => userAnswers => navigateFromCheckYourStateDetailsPage(CheckMode)(userAnswers)
     case ImportTypePage                    => userAnswers => navigateFromImportTypePage(CheckMode)(userAnswers)
     case ImportSubCodePage                 => _ => importRoutes.SadReferenceController.onPageLoad
+    case SadReferencePage                  => userAnswers =>
+      userAnswers.get(SadReferencePage) match {
+        case Some(true)  => importRoutes.SadReferenceNumberController.onPageLoad(CheckMode)
+        case Some(false) => controllers.routes.JourneyRecoveryController.onPageLoad()
+        case _           => controllers.routes.JourneyRecoveryController.onPageLoad()
+      }
+    case SadReferenceNumberPage            => _ => controllers.routes.JourneyRecoveryController.onPageLoad()
     case PurchaseTypePage                  => userAnswer => navigateFromPurchaseTypePage(CheckMode)(userAnswer)
     case PurchaseSubCategoryPage           => userAnswers => navigateFromPurchaseSubCategoryPage(CheckMode, userAnswers)
     case DescribeItemsOnInvoicePage        => _ => purchaseRoutes.CheckYourPurchaseDetailsController.onPageLoad()

conf/messages.cy
@@ -571,3 +571,10 @@ singleAdministrativeDocumentReferenceNumberAvailable.heading = Do you have a Sin
 singleAdministrativeDocumentReferenceNumberAvailable.hint = You can usually find this at the top of the import document
 sadReference.error.required = Select yes if you have the Single Administrative Document (SAD) reference number
 singleAdministrativeDocumentReferenceNumberAvailable.error.required = Select yes if you have the Single Administrative Document (SAD) reference number
+
+sadReferenceNumber.title = What is your Single Administrative Document (SAD) reference number?
+sadReferenceNumber.heading = What is your Single Administrative Document (SAD) reference number?
+sadReferenceNumber.hint = Enter your SAD reference number in the correct format
+sadReferenceNumber.error.required = Enter your SAD reference number in the correct format
+sadReferenceNumber.error.invalid = Enter your SAD reference number in the correct format
+sadReferenceNumber.error.length = SAD reference number must be 18 characters or less